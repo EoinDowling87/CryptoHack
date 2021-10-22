de23c043ede4e5509fcb0c66bec5a2bb5c64e065
EoinDowling34 <B00123313@mytudublin.ie>
2021-10-22 10:11:13 -0400

    challenges solved

--- general/lemur.py
+++ general/lemur.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageChops
-pic1 = Image.open("/home/b00123313/Desktop/CryptoHack/general/LemurXOR/lemar.png")
-pic2 = Image.open("/home/b00123313/Desktop/CryptoHack/general/LemurXOR/flag.png")
-pic3 = ImageChops.add(ImageChops.subtract(pic2, pic1), ImageChops.subtract(pic1, pic2))
-pic3.show()
-pic3.save("/home/b00123313/Desktop/CryptoHack/general/LemurXOR//lemrpic3.png")
+picture1 = Image.open("/home/b00123313/Desktop/CryptoHack/general/LemurXOR/lemar.png")
+picture2 = Image.open("/home/b00123313/Desktop/CryptoHack/general/LemurXOR/flag.png")
+picture3 = ImageChops.add(ImageChops.subtract(picture2, picture1), ImageChops.subtract(picture1, picture2))
+picture3.show()
+picture3.save("/home/b00123313/Desktop/CryptoHack/general/LemurXOR/picture3.png")
